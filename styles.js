bd3f80143c7b1b2cd1fafe92d531e51c675ebb15
tomari <t.omari@floranow.com>
2021-01-03 13:47:22 +0200

    change styles controlsContainer

--- styles.js
+++ styles.js
@@ -1,26 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   controlsContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flex: 1,
+    width: "100%",
+
+    flexDirection: "row",
+    alignItems: "center",
   },
   highThumbContainer: {
-    position: 'absolute',
+    position: "absolute",
   },
   railsContainer: {
+    flex: 1,
     ...StyleSheet.absoluteFillObject,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   labelFixedContainer: {
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
   },
   labelFloatingContainer: {
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     right: 0,
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
   },
   touchableArea: {
     ...StyleSheet.absoluteFillObject,
